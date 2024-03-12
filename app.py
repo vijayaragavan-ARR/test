7d851e6913c6bfec2b549c0e314431841eb5ebbd
Vijayaragavan ARR <136185806+vijayaragavan-ARR@users.noreply.github.com>
2024-03-12 23:51:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,37 @@
-
 from flask import Flask, request, jsonify
+from nltk.tokenize import RegexpTokenizer
+import nltk
 
+# Initialize Flask app
 app = Flask(__name__)
 
+# Download NLTK resources 
+nltk.download('punkt')       # Download punkt tokenizer resource
+nltk.download('averaged_perceptron_tagger')  # Download averaged_perceptron_tagger resource
+
+# Define auxiliary verbs and POS tags to remove
+auxiliary_verbs = {'be', 'am', 'is', 'are', 'was', 'were', 'been', 'being', 'have', 'has', 'had', 'do', 'does'}
+tags_to_remove = {'MD', 'PUNCT'}
+
+# Initialize RegexpTokenizer with a pattern to tokenize words while excluding punctuations
+tokenizer = RegexpTokenizer(r'\w+')
+
+# Define route for POST requests to '/'
 @app.route('/', methods=['POST'])
-def get_words():
-    # Get the input string from the request
+def get_filtered_words():
+    # Get input_string from the JSON payload of the POST request
     input_string = request.json.get('input_string')
-    # Split the input string into words
-    words = input_string.split()
 
-    # Return the list of words as JSON
-    return jsonify({'words': words})
+    # Tokenize input_string and perform part-of-speech tagging
+    words = tokenizer.tokenize(input_string)
+    tagged_words = nltk.pos_tag(words)
 
+    # Filter out words based on specified POS tags and auxiliary verbs
+    filtered_words = [word for word, tag in tagged_words if tag not in tags_to_remove and word.casefold() not in auxiliary_verbs]
+
+    # Return filtered words as JSON response
+    return jsonify({'filtered_words': filtered_words})
+
+# Run the Flask app
 if __name__ == '__main__':
     app.run(debug=True)
